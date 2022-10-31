ec0fd038f52205d6cd22221324acefc05701977b
calvinzxie <calvinxie@wustl.edu>
2022-10-31 17:46:17 -0500

    a

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-r = document.getElementById("id");
+image = document.getElementById("img1")
+image2 = document.getElementById("img2")
 
-function myfunc() {
-    console.log("hello");
+function enlarge() {
+    image.style.transform = "scale(1.3)";
+    image.style.transition = "transform 0.5s";
 }
+
+function resetSize() {
+    image.style.transform = "scale(1)";
+}
+
+function hoverEnlarge(){
+    image2.style.transform = "scale(1.5)"
+    image2.style.transition = "transform 0.5s";
+}
+
+
 
