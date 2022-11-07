b004bd3c7926e5614335bf33d48b6c53f2622669
calvinzxie <calvinxie@wustl.edu>
2022-11-07 04:35:13 -0600

    a

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var slideshow = ['img1','img2','img3','img4','img5'];
-var x = 0;
+var slideshow = ["images/img1.jpg","images/img2.jpg","images/img3.jpg","images/img4.jpg","images/img5.jpg"];
+var slideshowIndex = 0;
+var source;
 
 function view(image) {
     document.getElementById("largeImage").style.display = "block";
-    var source = image.src;
-    var image = document.getElementById("largeImage");
-    image.src = source;
+    source = image.src;
+    document.getElementById("largeImage").src = source;
+    console.log(source);
 }
 
 function hover(image) {
@@ -14,4 +15,20 @@ function hover(image) {
 
 function normal(image) {
     image.style.transform = "scale(1)";
+}
+
+function next() {
+    slideshowIndex++;
+    slideshowIndex = Math.abs(slideshowIndex);
+    slideshowIndex = slideshowIndex % slideshow.length;
+    var x = document.getElementById("largeImage").src;
+    document.getElementById("largeImage").src = slideshow[slideshowIndex];
+}
+
+function previous() {
+    slideshowIndex--;
+    slideshowIndex = Math.abs(slideshowIndex);
+    slideshowIndex = slideshowIndex % slideshow.length;
+    var x = document.getElementById("largeImage").src;
+    document.getElementById("largeImage").src = slideshow[slideshowIndex];
 }
