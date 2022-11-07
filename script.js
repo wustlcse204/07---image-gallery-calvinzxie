5a6531a8cd53147025ed9a3b29148e293afaa840
calvinzxie <calvinxie@wustl.edu>
2022-11-07 02:46:35 -0600

    a

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 var slideshow = ['img1','img2','img3','img4','img5'];
 var x = 0;
 
-function view(e) {
-    var img = document.getElementById(e);
-    var largeimg = document.getElementById("largeimg")
-    largeimg.src = e;
-    largeimg.style.display = "block";
-    largeimg.style.transform = "scale(2)";
-    largeimg.style.position = 'absolute';
-  
+function view(image) {
+    document.getElementById("largeImage").style.display = "block";
+    var source = image.src;
+    var image = document.getElementById("largeImage");
+    image.src = source;
+
 }
